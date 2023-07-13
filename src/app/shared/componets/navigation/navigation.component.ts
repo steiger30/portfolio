62c591520d06611b53540513dafbe444aefc4d0b
Renan Steiger <renansteiger@live.com>
2023-07-13 10:34:43 -0300

    feat: navigation responsive

--- src/app/shared/componets/navigation/navigation.component.ts
+++ src/app/shared/componets/navigation/navigation.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
+  show(){
+    document.querySelector('.hamburger')?.classList.toggle('open')
+    document.querySelector('.navigation')?.classList.toggle('active')
+  }
 }
